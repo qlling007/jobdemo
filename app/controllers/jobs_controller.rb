2864eefdcfe9700d18c3f7e44f7f660b9a6235bc
qinglingling <qlingling007@gmail.com>
2017-03-20 19:43:13 +0800

    implement part 2

--- app/controllers/jobs_controller.rb
+++ app/controllers/jobs_controller.rb
@@ -1,5 +1,6 @@
 class JobsController < ApplicationController
   before_action :authenticate_user!, only: [:new, :create, :update, :edit, :destroy]
+  
   def index
     @jobs = Job.all
 
@@ -41,7 +42,7 @@ class JobsController < ApplicationController
 end
   private
   def job_params
-    params.requier(:job).permit(:title, :description)
+   params.require(:job).permit(:title, :description)
 
   end
 end
